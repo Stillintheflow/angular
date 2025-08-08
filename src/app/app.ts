3fed39cfdc50cb91a46bc62ac8d35ea98b44b96f
Stillintheflow <songlasong1234@gmail.com>
2025-08-08 19:53:32 +0700

    init project

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Header } from './header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [Header],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('angular100-d-o-c');
+}
