6933d1567aca52e516613e0732b1b46eb9b32cb9
Stillintheflow <songlasong1234@gmail.com>
2025-08-10 23:36:42 +0700

    dinamic data for User component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './user.scss'
 })
 export class User {
+  slecetedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath(): string {
+    return `assets/users/${this.slecetedUser.avatar}`;
+  }
 }
