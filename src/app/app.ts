0b191e6098fbb5cf8f1a7c297ef634461b28bca1
Stillintheflow <songlasong1234@gmail.com>
2025-08-13 20:54:03 +0700

    update data with event  binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,24 @@ import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { User } from "./user/user";
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angular100-d-o-c');
   users = DUMMY_USERS;
+  slectedUserId = 'u1';
 
-  onSelectUser(id: string) {
-    console.log('Selected user ID:', id);
+  get selectedUser() {
+    return this.users.find(user => user.id === this.slectedUserId);
+  }
+
+  onSelectUserApp(id: string) {
+    this.slectedUserId = id;
   }
 }
